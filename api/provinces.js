6bc1f24ffa6c47eeef1a297073513f75812306b7
Aryan kholqi <aryankholqi@gmail.com>
2023-09-02 15:43:29 +0330

    First Next Project

--- api/provinces.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const getAllProvinces = ()=>{
-    return axios.get("https://iran-locations-api.vercel.app/api/v1/states")
-}
-export const getCenterOfProvince = (id)=>{
-    return axios.get(`https://iran-locations-api.vercel.app/api/v1/states?id=${id}`)
-}
--- querries/useProduct.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function useProduct() {
-  return (
-    <div>useProduct</div>
-  )
-}
